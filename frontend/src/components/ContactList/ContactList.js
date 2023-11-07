7c70406e82366c2ac60c75a77d74e17e5a661749
z <prospectheory@gmail.com>
2023-11-08 01:17:05 +0800

    refine table showing contact details

--- frontend/src/components/ContactList/ContactList.js
+++ frontend/src/components/ContactList/ContactList.js
@@ -18,7 +18,10 @@ export default function ContactList() {
   if (isLoading) return <div>Loading data</div>;
 
   return (
-    <table className={styles.table}>
+    <table
+      className={styles.table}
+      style={{ "--vertical-padding": "5px", "--horizontal-padding": "15px" }}
+    >
       <thead>
         <tr>
           <th>Id</th>
@@ -26,20 +29,59 @@ export default function ContactList() {
           <th>Last name</th>
           <th>Birthday</th>
           <th>Marital status</th>
+          <th>Emails</th>
         </tr>
       </thead>
       <tbody>
-        {data.map(({ id, first_name, last_name, birthday, marital_status }) => {
-          return (
-            <tr>
-              <td>{id} </td>
-              <td>{first_name} </td>
-              <td>{last_name} </td>
-              <td>{new Date(birthday).toLocaleDateString()} </td>
-              <td>{marital_status} </td>
-            </tr>
-          );
-        })}
+        {data.map(
+          ({ id, first_name, last_name, birthday, marital_status, emails }) => {
+            const count = 2;
+            return (
+              <React.Fragment key={id}>
+                <tr key={id}>
+                  <td
+                    className={`${styles.bottomPaddingBorder} ${styles.topPadding}`}
+                    rowSpan={count}
+                  >
+                    {id}
+                  </td>
+                  <td
+                    className={`${styles.bottomPaddingBorder} ${styles.topPadding}`}
+                    rowSpan={count}
+                  >
+                    {first_name}
+                  </td>
+                  <td
+                    className={`${styles.bottomPaddingBorder} ${styles.topPadding}`}
+                    rowSpan={count}
+                  >
+                    {last_name}
+                  </td>
+                  <td
+                    className={`${styles.bottomPaddingBorder} ${styles.topPadding}`}
+                    rowSpan={count}
+                  >
+                    {new Date(birthday).toLocaleDateString()}
+                  </td>
+                  <td
+                    className={`${styles.bottomPaddingBorder} ${styles.topPadding}`}
+                    rowSpan={count}
+                  >
+                    {marital_status}
+                  </td>
+                  <td className={styles.topPadding}>
+                    {emails[0]?.email_address}
+                  </td>
+                </tr>
+                <tr>
+                  <td className={styles.bottomPaddingBorder}>
+                    {emails[1]?.email_address}
+                  </td>
+                </tr>
+              </React.Fragment>
+            );
+          }
+        )}
       </tbody>
     </table>
   );
