76b73df3746b25acdfc524fa85fa8bd1aa83fcb3
z <prospectheory@gmail.com>
2023-12-12 17:43:56 +0800

    config for vercel deployment

---
+++ backend/api/index.js
@@ -0,0 +1,24 @@
+var express = require("express");
+var router = express.Router(); // gets an instance of express router
+const controllers = require("../controllers");
+
+const { contactController } = controllers;
+
+/* GET home page. */
+router.get("/", function (req, res, next) {
+  return res.send("hello homepage");
+});
+
+/* Contact router */
+router.get("/api/contacts", contactController.findAll);
+router.post("/api/contacts", contactController.addOne);
+router.put("/api/contacts/:userId", contactController.updateAll);
+// router.get("/api/contacts/:first_name", contactController.findByName);
+router.get("/api/contacts/:id", contactController.findById);
+router.delete("/api/contacts/delete-all", contactController.deleteAll);
+// router.delete("/api/contacts", contactController.deleteContact);
+router.delete("/api/contacts/:userId", contactController.deleteOne);
+
+// router.get("/api/test", contactController.testQuery);
+
+module.exports = router;
