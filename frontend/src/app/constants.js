f626b3ee62140665f34fdad032c8345604e785a0
z <prospectheory@gmail.com>
2023-12-15 10:40:20 +0800

    fix url bug

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -1,4 +1,4 @@
 export const BACKEND_URL =
   process.env.NODE_ENV === "production"
-    ? "https://fullstack-address-book.vercel.app"
+    ? "https://addressbookapp-holy-violet-9908.fly.dev"
     : "http://localhost:3000";
