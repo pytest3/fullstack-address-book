fcace0d3b3709bc88fd24683702e4ba7799bb79c
z <prospectheory@gmail.com>
2023-12-26 23:30:29 +0800

    remove un-used url

--- frontend/src/app/constants.js
+++ frontend/src/app/constants.js
@@ -3,7 +3,5 @@ export const BACKEND_URL =
     ? "https://addressbookapp-holy-violet-9908.fly.dev"
     : " https://f7d1-103-6-151-183.ngrok-free.app";
 
-export const FRONTEND_URL_NGROK = "https://5d10-103-6-151-183.ngrok-free.app";
-
 /* http://localhost:3000
 https://3038-103-6-151-183.ngrok-free.app */
