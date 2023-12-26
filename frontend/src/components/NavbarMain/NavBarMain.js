ab2e13de974db665bf4397133d663927b83c7747
z <prospectheory@gmail.com>
2023-12-27 01:15:28 +0800

    update logout icon

--- frontend/src/components/NavbarMain/NavBarMain.js
+++ frontend/src/components/NavbarMain/NavBarMain.js
@@ -1,13 +1,29 @@
 import React from "react";
 import styles from "./NavBarMain.module.css";
-import { Plus, UserCircle } from "lucide-react";
+import { Plus, UserCircle, LogOut, Trash2 } from "lucide-react";
 import NavBarWrapper from "../NavBarWrapper";
 import Link from "next/link";
-import { Trash2 } from "lucide-react";
 import Modal2 from "../Modal2";
 import { useHttp } from "@/hooks/useHttp";
 import { BACKEND_URL } from "@/app/constants";
 import { signOut } from "next-auth/react";
+
+<svg
+  xmlns="http://www.w3.org/2000/svg"
+  width="24"
+  height="24"
+  viewBox="0 0 24 24"
+  fill="none"
+  stroke="currentColor"
+  stroke-width="2"
+  stroke-linecap="round"
+  stroke-linejoin="round"
+  class="lucide lucide-log-out"
+>
+  <path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4" />
+  <polyline points="16 17 21 12 16 7" />
+  <line x1="21" x2="9" y1="12" y2="12" />
+</svg>;
 
 export default function NavBarMain({
   isEdit,
@@ -104,7 +120,10 @@ export default function NavBarMain({
           <Link href="/new-contact">
             <Plus className={styles.icon} />
           </Link>
-          <UserCircle className={styles.icon} onClick={signOut} />
+          <LogOut
+            className={`${styles.icon} ${styles.logOutIcon}`}
+            onClick={signOut}
+          />
         </div>
       </div>
     </NavBarWrapper>
